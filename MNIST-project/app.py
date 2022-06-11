7d0d6924434ddae3f8aaa4a17e09e2fdd146d960
Ivan Reyes <ivanrs297@gmail.com>
2022-06-11 18:57:01 -0500

    Change custom model path

--- MNIST-project/app.py
+++ MNIST-project/app.py
@@ -2,15 +2,18 @@ import jetson.inference
 import jetson.utils
 
 # load model 
-net = jetson.inference.imageNet(model = "simple_mnist_model.onnx")
+net = jetson.inference.imageNet(argv=['--model=simple_mnist_model.onnx'])
+# net = jetson.inference.detectNet(argv=[‘--model=models/coin/ssd-mobilenet.onnx’, ‘--labels=models/coin/labels.txt’, ‘--input-blob=input_0’, ‘--output-cvg=scores’, ‘--output-bbox=boxes’], threshold=0.5)
 camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
 display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
+font = jetson.utils.cudaFont()
 
 while display.IsStreaming():
     img = camera.Capture()
     
     # classify the image
     class_idx, confidence = net.Classify(img)
+    font.OverlayText(img, img.width, img.height, "{:05.2f}% {:s}".format(confidence * 100, class_desc), 5, 5, font.White, font.Gray40)
     display.Render(img)
     display.SetStatus("Classification | Network {:.0f} FPS".format(net.GetNetworkFPS()))
 
